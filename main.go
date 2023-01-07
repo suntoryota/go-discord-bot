84e9d8406d33d88e1b3f9abc77451dd3f3fa8594
suntoryota <ryotasunto@gmail.com>
2023-01-07 17:51:49 +0700

    go-discord-bot

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"go-discord-bot/bot"
+	"go-discord-bot/config"
+)
+
+func main() {
+	err := config.ReadConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
